6e77f36e87f1ad58b1d300d02ad5e09d24c8b5b2
adl.fahmia <adl.fahmia@xl.co.id>
2024-09-15 09:42:32 +0700

    fix media

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,78 +1,103 @@
 "use client";
+// pages/video.js
+import React, { useEffect, useRef, useState } from "react";
 import { useGlobalContext } from "@/context/global";
 import styles from "./Login.module.css";
 
 const LoginPage: React.FC = () => {
-  const { innerWidth } = useGlobalContext();
-  return (
-    <div className={styles.container}>
-      <div
-        style={{
-          position: innerWidth > 768 ? "relative" : "absolute",
-          zIndex: 10,
-          flex: 1,
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "0px 20px",
-          //   backgroundColor: "#170027",
-        }}
-      >
-        <form className={styles.form}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              alignItems: "center",
-            }}
-          >
-            <img
-              src="/aigptku.id-white1.png"
-              style={{
-                width: "40%",
-              }}
-              alt=""
-            />
-          </div>
-          <input
-            type="email"
-            placeholder="Email"
-            className={styles.input}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className={styles.input}
-            required
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              gap: 10,
-            }}
-          >
-            <button type="submit" className={styles.button}>
-              Masuk
-            </button>
-            <button type="button" className={styles.button_register}>
-              Daftar
-            </button>
-          </div>
-        </form>
-      </div>
+  const { innerWidth, contextLoading } = useGlobalContext();
+  const [showPopup, setShowPopup] = useState<boolean>(true);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
-      <Video />
-    </div>
+  useEffect(() => {}, [contextLoading]);
+  return (
+    !contextLoading && (
+      <div className={styles.container}>
+        <div
+          style={{
+            // backgroundColor: "red",
+            position: innerWidth > 768 ? "relative" : "absolute",
+            top: 0,
+            zIndex: 10,
+            flex: 1,
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "0px 20px",
+            //   backgroundColor: "#170027",
+          }}
+        >
+          <form className={styles.form}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src="/aigptku.id-white1.png"
+                style={{
+                  width: "40%",
+                }}
+                alt=""
+              />
+            </div>
+            <input
+              type="email"
+              placeholder="Email"
+              className={styles.input}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className={styles.input}
+              required
+            />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                gap: 10,
+              }}
+            >
+              <button type="submit" className={styles.button}>
+                Masuk
+              </button>
+              <button type="button" className={styles.button_register}>
+                Daftar
+              </button>
+            </div>
+          </form>
+        </div>
+        <Video videoRef={videoRef} />
+        {showPopup && (
+          <FirstTimePage
+            action={() => {
+              localStorage.setItem("firstTime", "N");
+              setShowPopup(false);
+              if (videoRef?.current) {
+                videoRef.current?.play();
+              } else {
+                setTimeout(() => {
+                  videoRef.current?.play();
+                }, 1000);
+              }
+            }}
+          />
+        )}
+      </div>
+    )
   );
 };
 
-// pages/video.js
-import React, { useEffect } from "react";
-
-const Video = () => {
+const Video = ({
+  videoRef,
+}: {
+  videoRef: React.MutableRefObject<HTMLVideoElement | null>;
+}) => {
   const { innerWidth } = useGlobalContext();
   return (
     <>
@@ -83,7 +108,8 @@ const Video = () => {
             // backgroundColor: "red",
             width: "100%",
             height: "100%",
-            position: "absolute",
+            position: "fixed",
+            top: 0,
             zIndex: 1,
           }}
         ></div>
@@ -104,9 +130,9 @@ const Video = () => {
               }
             : {
                 position: "fixed",
+                top: 0,
                 zIndex: 0,
-                transform:
-                  "perspective(300px) rotateY(15deg) scale(3) translateX(-25%)",
+                transform: "scale(2)",
                 opacity: 0.2,
                 flex: 1,
                 height: "100%",
@@ -117,12 +143,55 @@ const Video = () => {
               }
         }
       >
-        <video autoPlay muted loop>
+        <video ref={videoRef} muted loop playsInline>
           <source src="/login1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
     </>
+  );
+};
+
+const FirstTimePage = ({ action }: { action: () => void }) => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        zIndex: 11,
+        top: 0,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 30,
+        backgroundColor: "#10001b",
+        color: "white",
+      }}
+    >
+      <img
+        src="/aigptku.id-white1.png"
+        style={{
+          width: "40%",
+        }}
+        alt=""
+      />
+      <button
+        type="button"
+        onClick={action}
+        style={{
+          // backgroundColor: "#056700",
+          border: "2px solid white",
+          color: "white",
+          padding: "0.8rem",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }}
+      >
+        Masuk / Daftar
+      </button>
+    </div>
   );
 };
 
